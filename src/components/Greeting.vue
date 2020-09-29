564c6f7999cb85eca4e634cbbbd35903b7ca4d29
Yannic Inselmann <yannic.inselmann@gmail.com>
2020-09-29 22:36:47 +0200

    feat: time specific greeting

--- src/components/Greeting.vue
+++ src/components/Greeting.vue
@@ -1,12 +1,12 @@
 <template>
   <h1>
-    <span class="hello">Hello, </span>
+    <span class="hello">{{ greeting }}, </span>
     <span class="name">{{ name || "Stranger" }}</span>
   </h1>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, onMounted, ref } from "vue";
 import { useStore } from "vuex";
 
 export default defineComponent({
@@ -14,8 +14,31 @@ export default defineComponent({
   setup() {
     const store = useStore();
     const name = computed(() => store.state.settings.name);
+
+    const getGreeting = () => {
+      const now = new Date().getHours();
+      if (now >= 6 && now < 11) {
+        return "Good morning";
+      } else if (now >= 11 && now < 13) {
+        return "Hello";
+      } else if (now >= 13 && now < 18) {
+        return "Good afternoon";
+      } else {
+        return "Good night";
+      }
+    };
+
+    const greeting = ref(getGreeting());
+
+    onMounted(() => {
+      setInterval(() => {
+        greeting.value = getGreeting();
+      }, 1000 * 60);
+    });
+
     return {
-      name
+      name,
+      greeting
     };
   }
 });
